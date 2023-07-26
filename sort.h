ad6e4d3f2eaaad2f7dc52837531b4291f0631c5e
Brian Omondi <omondiibrian00@gmail.com>
2023-07-26 21:58:13 +0300

    Shell sort

--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 listint_t *swap_node(listint_t *temp, listint_t **list);
+void shell_sort(int *array, size_t size);
 
 #endif
